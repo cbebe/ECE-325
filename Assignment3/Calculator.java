f9a9040c4d5799ab53d40b907db7eff84dff2d7b
Charles Ancheta <cancheta@ualberta.ca>
2020-10-14 23:21:08 -0600

    super stumped

--- Assignment3/Calculator.java
+++ Assignment3/Calculator.java
@@ -10,7 +10,6 @@ class RuntimeError extends Exception {
   }
 }
 
-
 class SyntaxError extends Exception {
   private static final long serialVersionUID = 1L;
 
@@ -18,7 +17,6 @@ class SyntaxError extends Exception {
     super(message);
   }
 }
-
 
 /**
  * ECE 326 - Fall 2020 <br/>
@@ -31,30 +29,14 @@ class SyntaxError extends Exception {
 public class Calculator {
 
   /**
-   * Evaluates a string with purely binary operations Using PEMAS as the order of operations (no
-   * division)
+   * Evaluates a string with purely binary operations Using PEMAS as the order of
+   * operations (no division)
    * 
    * @param operations {@code String} string to be evaluated
    * @return {@code int} value of the expression
    */
   int evalOperations(String operations) throws RuntimeError, SyntaxError {
-    Stack<Integer> valueStack = new Stack<Integer>();
-    Stack<Character> operationsStack = new Stack<Character>();
     int returnValue = 0;
-    String[] tokens = operations.split(" ");
-
-    for (String s : tokens) {
-      if (Pattern.matches("\\d+", s)) {
-        valueStack.push(Integer.parseInt(s));
-      } else if (Pattern.matches("\\+|-|^|\\*", s)) {
-        operationsStack.push(s.charAt(0));
-      } else if (Pattern.matches("let", s)) {
-        // start reading here
-      }
-
-    }
-
-
 
     return returnValue;
   }
@@ -67,15 +49,14 @@ public class Calculator {
    */
   public int execExpression(String exp) {
     int returnValue = 0;
-    // TODO: Assignment 4 Part 1 -- parse, calculate the expression, and return the correct
-    // value
-    var operationStack = new Stack<String>();
     var variableMap = new HashMap<String, Integer>();
+    var operationStack = new Stack<String>();
+    var bracketStack = new Stack<String>();
+    // TODO: Assignment 4 Part 1 -- parse, calculate the expression, and return the
+    // correct value
 
-    // TODO: Assignment 4 Part 2-1 -- when come to illegal expressions, raise proper exceptions
-    System.out.println(operationStack);
-    System.out.println(variableMap);
-
+    // TODO: Assignment 4 Part 2-1 -- when come to illegal expressions, raise proper
+    // exceptions
 
     return returnValue;
   }
@@ -88,24 +69,23 @@ public class Calculator {
   public static void main(String[] args) {
     Calculator calc = new Calculator();
     // Part 1
-    String[] inputs = {"let x = 1;", // 1, returns 1
+    String[] inputs = { "let x = 1;", // 1, returns 1
         "(let x = 1) + x;", // 2, returns 2
         "(let a = 2) + 3 * a - 5;", // 3, returns 3
         "(let x = (let y = (let z = 1))) + x + y + z;", // 4, returns 4
-        "1 + (let x = 1) + (let y = 2) + (1 + x) * (1 + y)" + " - (let x = y) - (let y = 1) - x;", // 5,
-                                                                                                   // returns
-                                                                                                   // 5
+        "1 + (let x = 1) + (let y = 2) + (1 + x) * (1 + y) - (let x = y) - (let y = 1) - x;", // 5,
+                                                                                              // returns
+                                                                                              // 5
         "1 + (let a = (let b = 1) + b) + a + 1;", // 6, returns 6
         "(let a = (let a = (let a = (let a = 2) + a) + a) + a) - 9;", // 7, returns 7
         "(let x = 2) ^ (let y = 3);", // 8, returns 8
         "(let y = 3) ^ (let x = 2);" // 9, returns 9
     };
     for (int i = 0; i < inputs.length; i++)
-      System.out.println(
-          String.format("%d -- %-90s %d", i + 1, inputs[i], calc.execExpression(inputs[i])));
+      System.out.println(String.format("%d -- %-90s %d", i + 1, inputs[i], calc.execExpression(inputs[i])));
 
     // Part 2
-    inputs = new String[] {"1 + (2 * 3;", // 1, syntax error: ')' expected
+    inputs = new String[] { "1 + (2 * 3;", // 1, syntax error: ')' expected
         "(let x 5) + x;", // 2, syntax error: '=' expected
         "(let x = 5) (let y = 6);", // 3, syntax error: operator expected
         "(let x = 5 let y = 6);", // 4, syntax error: ')' expected
@@ -114,8 +94,7 @@ public class Calculator {
     };
     // TODO: Assignment 3 Part 2-2 -- catch and deal with your exceptions here
     for (int i = 0; i < inputs.length; i++)
-      System.out.println(
-          String.format("%d -- %-30s %d", i + 1, inputs[i], calc.execExpression(inputs[i])));
+      System.out.println(String.format("%d -- %-30s %d", i + 1, inputs[i], calc.execExpression(inputs[i])));
   }
 
 }
