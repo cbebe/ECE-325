25cb037e0f94080a72266b10a4a832732b21b760
Charles Ancheta <cancheta@ualberta.ca>
2020-11-02 03:34:57 -0700

    add documentation

--- Assignment6/src/main/java/ece325/Song.java
+++ Assignment6/src/main/java/ece325/Song.java
@@ -9,12 +9,22 @@ public class Song {
     String title;
     double length;
 
+    /**
+     * Constructor for the {@code Song} class
+     * 
+     * @param artist {@code String} the artist of the song
+     * @param title  {@code String} the title of the song
+     * @param length {@code double} the length of the song
+     */
     public Song(String artist, String title, double length) {
         this.artist = artist;
         this.title = title;
         this.length = length;
     }
 
+    /**
+     * Getters for each field
+     */
     public String getArtist() {
         return artist;
     }
@@ -27,14 +37,34 @@ public class Song {
         return length;
     }
 
+    /**
+     * @param artist {@code String}
+     * @return {@code boolean} true if the artist matches this artist, ignoring case
+     */
     public boolean isArtist(String artist) {
         return this.artist.toLowerCase().equals(artist.toLowerCase());
     }
 
+    /**
+     * @param title {@code String}
+     * @return {@code boolean} true if the title matches this title, ignoring case
+     */
     public boolean isTitle(String title) {
         return this.title.toLowerCase().equals(title.toLowerCase());
     }
 
+    /**
+     * @return {@code String} String with format title - artist - length
+     */
+    public String toString() {
+        return String.format("%s - %s - %s", title, artist, length);
+    }
+
+    /**
+     * Override of Object.equals for {@code Song} class
+     * 
+     * @return {@code boolean} if the object matches all of song's fields
+     */
     @Override
     public boolean equals(Object obj) {
         if (obj == this)
