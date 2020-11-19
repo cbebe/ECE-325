9574368952ac96f5778fb671a573ad14d1d318c7
Charles Ancheta <cancheta@ualberta.ca>
2020-11-18 17:03:30 -0700

    doneeeeeeeee

--- Assignment7/Parts1-2/ArrayExample.java
+++ Assignment7/Parts1-2/ArrayExample.java
@@ -10,11 +10,6 @@ public class ArrayExample {
      * @param args {@code String[]} Command line arguments
      */
     public static void main(String[] args) {
-        // TODO: Assignment 7 Part 1-1
-        // You should add statements to this code as needed, so that
-        // the code compiles correctly; and when it is running, it raises
-        // a java.lang.ArrayStoreException for adding the cone to geoshapes.
-        // (Adding the circle should be OK.)
         GeometricShape[] geoshapes = new Circle[2];
 
         geoshapes[0] = new Circle(1.0); // Make this line to compile correctly
